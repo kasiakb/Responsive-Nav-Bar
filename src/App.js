ba615614d96325f36aaa8c0828d986fe81e89ad2
kasia brzozowicz <k.m.brzozowicz@gmail.com>
2018-07-13 18:00:30 +0200

    Display navigation list after click on navigaton button

--- src/App.js
+++ src/App.js
@@ -2,10 +2,34 @@ import React, { Component } from 'react';
 import TopNav from './TopNav';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.displayNavBar = this.displayNavBar.bind(this)
+    this.addClassName = this.addClassName.bind(this)
+
+    this.state = {
+      navButtonIsClicked: false,
+    };
+  }
+
+  displayNavBar(e) {
+    e.preventDefault();
+    this.setState({navButtonIsClicked: !this.state.navButtonIsClicked})
+  }
+
+  addClassName(navButtonIsClicked) {
+    return (newClassName, nullOrCurrent) => navButtonIsClicked ? newClassName : nullOrCurrent
+  }
+
   render() {
+    console.log(this.state.navButtonIsClicked, this.addClassName(this.state.navButtonIsClicked))
     return (
       <div>
-        <TopNav/>
+        <TopNav
+          displayNavBar = {this.displayNavBar}
+          addClassName = {this.addClassName(this.state.navButtonIsClicked)}
+        />
       </div>
     );
   }
--- src/TopNav.js
+++ src/TopNav.js
@@ -5,10 +5,11 @@ class TopNav extends Component {
   render() {
     return (
       <div className="nav">
-        <a className="nav_button" href="#nav" tabindex="1">
+        <a className="nav_button" href="#nav" tabindex="1"  onClick={(e)=>this.props.displayNavBar(e)}>
           <span className="nav--text ">Navigation</span>
+          <span className={this.props.addClassName('nav-triangleDown', 'nav-triangleUp')}></span>
         </a>
-        <ol className="nav_list">
+        <ol className ={ `nav_list ${this.props.addClassName('nav_list--display', '')}`}>
           <li className="nav_item">
             <a href="#" tabindex="1" class="nav_link nav--underline">
               Team
