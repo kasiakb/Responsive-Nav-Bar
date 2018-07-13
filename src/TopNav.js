cfd965b7d510854020a35799b80154d3feb4e97a
kasia brzozowicz <k.m.brzozowicz@gmail.com>
2018-07-13 12:24:17 +0200

    Create top navigation bar

---
+++ src/TopNav.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+class TopNav extends Component {
+  render() {
+    return (
+      <div className="nav">
+        <a className="nav_button" href="#nav" tabindex="1">
+          <span className="nav--text ">Navigation</span>
+        </a>
+        <ol className="nav_list">
+          <li className="nav_item">
+            <a href="#" tabindex="1" class="nav_link nav--underline">
+              Team
+            </a>
+          </li>
+          <li className="nav_item">
+            <a href="#" tabindex="1" className="nav_link nav--underline">
+              What We Do
+            </a>
+          </li>
+          <li className="nav_item">
+            <a href="#" tabindex="1" className="nav_link nav--underline">
+              Work
+            </a>
+          </li>
+          <li className="nav_item">
+            <a href="#" tabindex="1" className="nav_link nav--underline">
+              Foundry
+            </a>
+          </li>
+          <li className="nav_item">
+            <a href="#" tabindex="1" className="nav_link nav--underline">
+              Labs
+            </a>
+          </li>
+          <li className="nav_item">
+            <a href="#" tabindex="1" className="nav_link nav--box">
+              Contact
+            </a>
+          </li>
+        </ol>
+      </div>  
+    );
+  }
+}
+
+export default TopNav;
